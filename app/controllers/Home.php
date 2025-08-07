0c1938e58664ea131ffae1c82831168bad721578
SinethMendis <mendissineth03@gmail.com>
2025-08-07 17:10:48 +0530

    Remove debug output from Home controller
    Eliminated commented and direct echo statements from the Home controller's index method to clean up the code and prevent unintended output.

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,8 +4,6 @@ class Home extends Controller
 {
     public function index()
     {
-        // $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']['email'];
-        echo 'Called the home controller';
         $this->view('home');
     }
 
