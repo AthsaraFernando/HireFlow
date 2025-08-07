f74080d9331607e3f199a4ce385c23f8e1cd8507
SinethMendis <mendissineth03@gmail.com>
2025-08-07 17:06:50 +0530

    Add role-based redirect after sign in
    Implemented logic in the Signin controller to redirect users to different dashboards based on their role after successful sign in. If the role is unrecognized, users are redirected to a 404 page. Errors from the sign-in process are passed to the view.

--- app/controllers/Signin.php
+++ app/controllers/Signin.php
@@ -2,5 +2,35 @@
 
 class Signin extends Controller
 {
-    
+    public function index()
+    {
+        $data = [];
+        $user = new User;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($user->signInValidate($_POST, $user)) {
+                $user_role = (int) $_SESSION['USER']['role_id'];
+                switch ($user_role) {
+                    case 1:
+                        redirect('systemadmin/systemadmin');
+                        exit(); 
+                    case 2:
+                        redirect('hradmin/hradmin');
+                        exit();
+                    case 3:
+                        redirect('recruitmanager/recruitmanager');
+                        exit();
+                    case 4:
+                        redirect('applicant/applicant');
+                        exit();
+                    default:
+                        redirect('404');
+                        exit();
+                }
+            }
+        }
+
+        $data['errors'] = $user->errors;
+        $this->view('home', $data);
+    }
+
 }
