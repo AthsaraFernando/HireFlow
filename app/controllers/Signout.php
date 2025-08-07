eff2c6931897c455676bae0e5e32ba0bd3653c78
SinethMendis <mendissineth03@gmail.com>
2025-08-07 17:07:38 +0530

    Implement signout functionality in Signout controller
    Added an index method to the Signout controller that unsets the USER session variable if it exists and redirects to the home page.

--- app/controllers/Signout.php
+++ app/controllers/Signout.php
@@ -1,5 +1,14 @@
 <?php
 
-class Signout extends Controller{
-    
+class Signout extends Controller
+{
+    public function index()
+    {
+        if (!empty($_SESSION['USER'])) {
+            unset($_SESSION['USER']);
+        }
+        redirect('home');
+    }
+
+
 }
