42b38de8e5d39ab9b5cf001a07692a61d8e4efa2
SinethMendis <mendissineth03@gmail.com>
2025-08-07 17:18:40 +0530

    Add index method to Dashboard controller
    Introduces an index() method in the Dashboard controller to prepare and pass role data to the 'systemadmin' view as dummy data.

--- app/controllers/systemadmin/Dashboard.php
+++ app/controllers/systemadmin/Dashboard.php
@@ -1,7 +1,21 @@
 <?php
 
-class Dashboard extends Controller // or SystemAdmin
+class Dashboard extends Controller
 {
 
+    public function index()
+    {
+        $URL['view'] = 'dashboard';
 
+        // $user = new User;
+        // $rows = $user->findAll();
+        // $URL['users'] = is_array($rows) && count($rows) > 0 ? $rows : [];
+        $role = new Role;
+        $rows = $role->findAll();
+        $URL['roles'] = is_array($rows) && count($rows) > 0 ? $rows : [];
+
+        // show($URL);
+
+        $this->view('systemadmin', $URL);
+    }
 }
