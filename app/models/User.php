1d0ead7e76971645906af1c871ccc0bc43d602c4
SinethMendis <mendissineth03@gmail.com>
2025-08-07 17:31:33 +0530

    Enhance User model validation and authentication
    Updated the User model to use the correct table name 'users' and expanded allowed columns. Split validation into signUpValidate and signInValidate methods, adding stricter password requirements, first and last name checks, password confirmation, and role-based authentication during sign-in.

--- app/models/User.php
+++ app/models/User.php
@@ -3,15 +3,25 @@
 class User
 {
     use Model;
-    protected $table = 'user';
+    protected $table = 'users';
     protected $allowedColumns = [
+        'full_name',
         'email',
         'password',
+        'role_id'
     ];
 
-    public function validate($data)
+    public function signUpValidate($data)
     {
         $this->errors = [];
+
+        if (empty($data['first_name'])) {
+            $this->errors['first_name'] = "First name is required";
+        }
+
+        if (empty($data['last_name'])) {
+            $this->errors['last_name'] = "Last name is required";
+        }
 
         if (empty($data['email'])) {
             $this->errors['email'] = "Email is required";
@@ -19,18 +29,47 @@ class User
             $this->errors['email'] = "Email is not valid";
         }
 
-        if (empty($data['password'])) {
-            $this->errors['password'] = "Password is required";
+        if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $data['password'])) {
+            $this->errors['password'] = "Password must be at least 8 characters and include uppercase, lowercase, number, and special character";
         }
 
-        if (empty($data['terms'])) {
-            $this->errors['terms'] = "Please accept the terms and conditions";
+        if ($data['password'] !== $data['confirm_password']) {
+            $this->errors['password'] = "Passwords do not match";
         }
 
         if (empty($this->errors)) {
             return true;
         }
         return false;
+    }
+
+    public function signInValidate($data, $user)
+    {
+        $arr['email'] = $data['email'];
+        $row = $user->first($arr, []);
+        if ($row) {
+
+            $posted_role_id = (int) $data['role_id'];
+            $db_role_id = (int) $row['role_id'];
+
+            if ($row['password'] === $_POST['password'] && $db_role_id === $posted_role_id) {
+                $_SESSION['USER'] = $row;
+                return true;
+            } else {
+                if ($db_role_id !== $posted_role_id) {
+                    $user->errors['role_id'] = "Invalid user type";
+                }
+                if ($row['password'] !== $_POST['password']) {
+                    $user->errors['password'] = "Invalid password";
+                }
+            }
+        } else {
+            $user->errors['email'] = "Invalid email";
+        }
+
+        return false;
+
+
     }
 
 }
